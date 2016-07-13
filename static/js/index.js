5913fe85c93dd0455dc787620c42f74254c983c5
snpefk <snpefk@gmail.com>
2016-07-14 00:06:02 +0700

    spartan version

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,53 @@
 $(document).ready(function () {
-    var url = 'https://raw.githubusercontent.com/Snpefk/books/gh-pages/qst.xml';
+    var url = 'https://raw.githubusercontent.com/Snpefk/books/gh-pages/static/qst.xml',
+        root,
+        $content = $('#content'),
+        $input = $('input');
 
     $.get(url, function (data) {
         var xml = $.parseXML(data);
-        var root = xml.children[0];
+        root = xml.children[0];
         initialize(root);
     });
 
     function print(text) {
-        $('body').append('<p>' + text + '</p>');
+        $content.append('<p>' + text + '</p>');
+    }
+
+    function clear() {
+        $content.html('');
+    }
+
+    function clearInput() {
+        $input.val('');
     }
 
     function initialize(root) {
         print(root.getAttribute('title'));
         getChildren(root);
     }
-    
+
     function getChildren(node) {
         var children = node.children;
 
+        clear();
         for (var i = 0; i < children.length; ++i) {
             var title = children[i].getAttribute('title');
-            $('body').append('<p>' + (i + 1) + " - " + title + '</p>');
+            var text = (i + 1) + ' - ' + title;
+            print(text);
         }
     }
 
-    
+    $('input').keypress(function (e) {
+        if (e.keyCode == 13) {
+            var selectedNumber = this.value;
+            root = root.children[selectedNumber - 1];
+            getChildren(root);
+            clearInput();
+
+        }
+    })
+
+
 });
 
