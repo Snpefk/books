87c6df7e6ad8a87c1395f359e7a3665c56c164ac
snpefk <snpefk@gmail.com>
2016-07-16 01:18:38 +0700

    update frontend

--- static/js/index.js
+++ static/js/index.js
@@ -2,78 +2,90 @@ $(document).ready(function () {
     var url = 'https://raw.githubusercontent.com/Snpefk/books/gh-pages/static/qst.xml',
         root,
         $content = $('#content'),
-        $input = $('input');
+        $book = $('#book');
 
     $.get(url, function (data) {
         var xml = $.parseXML(data);
         root = xml.children[0];
-        initialize(root);
+
+        printNode(root);
+        printAnswers(root.children);
     });
 
-    function getColor(type) {
+    function clear() {
+        $content.html('');
+        $book.html('');
+    }
+
+    function getAnswerColor(type) {
         switch (type) {
-            case 'node':
-                return 'btn-default';
             case 'true':
                 return 'btn-success';
             case 'false':
                 return 'btn-danger';
             case 'maybe':
                 return 'btn-info';
-            case 'book':
-                return 'btn-primary';
             default:
                 return 'btn-default';
         }
     }
 
-    function print(text, type) {
-        if (type == 'node') {
-            
-        }
-        var color = "btn " + getColor(type);
-        var button = document.createElement('button');
-        button.className = color+' btn-block';
-        button.innerHTML = text;
-        button.setAttribute('type', 'button');
-        $content.append(button);
+    function printNode(children) {
+        // for (var i = 0; i < children.length; i++) {
+        var title = children.getAttribute('title');
+        $('#menu').html(title);
+        // }
     }
 
-    function clear() {
-        $content.html('');
-    }
-
-    function clearInput() {
-        $input.val('');
-    }
-
-    function initialize(root) {
-        print(root.getAttribute('title'), root.tagName);
-        getChildren(root);
-    }
-
-    function getChildren(node) {
-        var children = node.children;
-
+    function printAnswers(children) {
         for (var i = 0; i < children.length; ++i) {
             var title = children[i].getAttribute('title');
-            var type = children[i].tagName == 'answer' ? children[i].getAttribute('type') : children[i].tagName; // уберу
-            var text = (i + 1) + ' - ' + title;
-            print(text, type);
+            var type = children[i].getAttribute('type');
+            var color = "btn " + getAnswerColor(type);
+            var button = document.createElement('button');
+
+            button.className = color + ' btn-block';
+            button.innerHTML = title ? title : ' '; //проверка, если есть ли текст в блоке
+            button.id = i;
+            button.setAttribute('type', 'button');
+
+            $content.append(button);
         }
     }
 
-    $input.keypress(function (e) {
-        if (e.keyCode == 13) {
-            var selectedNumber = this.value;
-            root = root.children[selectedNumber - 1];
+    function printBook(children) {
+        for (var i = 0; i < children.length; ++i) {
+            var title = children[i].getAttribute('title');
+            var author = children[i].getAttribute('author');
+            var img = children[i].getAttribute('img');
+            var h3 = document.createElement('h3');
+            var h2 = document.createElement('h2');
 
-            clear();
-            clearInput();
-            getChildren(root);
+            h3.innerHTML = author;
+            h2.innerHTML = title;
+
+            $book.css('display', 'block').append(h2).append(h3);
+        }
+    }
+
+    $(document).on('click', 'button', function (e) {
+        var id = $(this).attr('id');
+        root = root.children[id];
+
+        clear();
+        if (root.children.length > 0) {
+            while (root.children[0].tagName != 'answer') {
+                if (root.children[0].tagName == 'node') {
+                    printNode(root.children[0]);
+                    root = root.children[0];
+                } else if (root.children[0].tagName == 'book') {
+                    printBook(root.children);
+                    root = root.children[0];
+                    break;
+                }
+            }
+            printAnswers(root.children);
         }
     })
-
-
 });
 
